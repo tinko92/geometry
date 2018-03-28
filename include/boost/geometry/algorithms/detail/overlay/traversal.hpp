ec5ca31fb31de189f068c16938e756a68f56207d
Barend Gehrels <barend@xs4all.nl>
2018-03-28 11:55:15 +0200

    [traversal] support linked clusters, which can handled differently (more simple). They occur more if rescaling is removed and fractions (segment ratios) are double values instead of ratios.

--- include/boost/geometry/algorithms/detail/overlay/traversal.hpp
+++ include/boost/geometry/algorithms/detail/overlay/traversal.hpp
@@ -103,6 +103,21 @@ template
 >
 struct traversal
 {
+private :
+    struct linked_turn_op_info
+    {
+        explicit linked_turn_op_info(signed_size_type ti = -1, int oi = -1,
+                    signed_size_type nti = -1)
+            : turn_index(ti)
+            , op_index(oi)
+            , next_turn_index(nti)
+        {}
+
+        signed_size_type turn_index;
+        int op_index;
+        signed_size_type next_turn_index;
+    };
+
     static const operation_type target_operation = operation_from_overlay<OverlayType>::value;
 
     typedef typename side_compare<target_operation>::type side_compare_type;
@@ -116,6 +131,7 @@ struct traversal
             point_type, SideStrategy, side_compare_type
         > sbs_type;
 
+public :
     inline traversal(Geometry1 const& geometry1, Geometry2 const& geometry2,
             Turns& turns, Clusters const& clusters,
             RobustPolicy const& robust_policy, SideStrategy const& strategy,
@@ -710,6 +726,90 @@ struct traversal
         return false;
     }
 
+    // Function checks simple cases, such as a cluster with two turns,
+    // arriving at the first turn, first turn points to second turn,
+    // second turn points further.
+    inline bool select_turn_from_cluster_linked(signed_size_type& turn_index,
+            int& op_index,
+            std::set<signed_size_type> const& ids) const
+    {
+        typedef typename std::set<signed_size_type>::const_iterator sit_type;
+
+        std::vector<linked_turn_op_info> possibilities;
+        std::set<signed_size_type> blocked_turns;
+        for (sit_type it = ids.begin(); it != ids.end(); ++it)
+        {
+            signed_size_type cluster_turn_index = *it;
+            turn_type const& cluster_turn = m_turns[cluster_turn_index];
+            if (cluster_turn.discarded)
+            {
+                continue;
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                turn_operation_type const& op = cluster_turn.operations[i];
+                turn_operation_type const& other_op = cluster_turn.operations[1 - i];
+                signed_size_type const ni = op.enriched.get_next_turn_index();
+                if (op.operation == target_operation
+                    || op.operation == operation_continue)
+                {
+                    possibilities.push_back(
+                        linked_turn_op_info(cluster_turn_index, i, ni));
+                }
+                else if (op.operation == operation_blocked
+                         && ! (ni == other_op.enriched.get_next_turn_index())
+                         && ids.count(ni) == 0)
+                {
+                    // Points to turn, not part of this cluster,
+                    // and that way is blocked. But if the other operation
+                    // points at the same turn, it is still fine.
+                    blocked_turns.insert(ni);
+                }
+            }
+        }
+
+        if (! blocked_turns.empty())
+        {
+            for (typename std::vector<linked_turn_op_info>::const_iterator it = possibilities.begin();
+                 it != possibilities.end(); ++it)
+            {
+                linked_turn_op_info const& lti = *it;
+                if (blocked_turns.count(lti.next_turn_index) > 0)
+                {
+                    return false;
+                }
+            }
+        }
+
+        if (possibilities.size() != 2)
+        {
+            // TODO: we might support longer links. For now support only two records
+            return false;
+        }
+
+        // Order it (2 records can just be reversed)
+        if (possibilities.front().next_turn_index != possibilities.back().turn_index)
+        {
+            std::reverse(possibilities.begin(), possibilities.end());
+        }
+
+        // The cluster is linked in right order.
+        // Traveral can either enter the cluster in the first turn,
+        // or it can start halfway.
+
+        bool const linked = possibilities.front().next_turn_index == possibilities.back().turn_index;
+        bool const all_same = possibilities.front().next_turn_index == possibilities.back().next_turn_index;
+        if (! (linked || all_same))
+        {
+            return false;
+        }
+
+        turn_index = possibilities.back().turn_index;
+        op_index = possibilities.back().op_index;
+
+        return true;
+    }
+
     inline bool select_turn_from_cluster(signed_size_type& turn_index,
             int& op_index,
             signed_size_type start_turn_index, int start_op_index,
@@ -725,6 +825,11 @@ struct traversal
 
         cluster_info const& cinfo = mit->second;
         std::set<signed_size_type> const& ids = cinfo.turn_indices;
+
+        if (select_turn_from_cluster_linked(turn_index, op_index, ids))
+        {
+            return true;
+        }
 
         sbs_type sbs(m_strategy);
 
