dc8da7e516ba091bc947941953388092a11861ee
Barend Gehrels <barend@xs4all.nl>
2018-04-25 09:16:55 +0200

    [test] rename testfile

--- test/robustness/overlay/areal_areal/general_intersection_precision.cpp
+++ test/robustness/overlay/areal_areal/general_intersection_precision.cpp
@@ -7,9 +7,6 @@
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#include "debug_sort_by_side_svg.hpp"
-
-
 #include <iostream>
 #include <iomanip>
 #include <fstream>
