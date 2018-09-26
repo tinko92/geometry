76581a7f4821b0ee12538e8163c9f4f52e4902c1
Barend Gehrels <barend@xs4all.nl>
2018-09-26 16:11:09 +0200

    [validity] skip adjacent segments for self intersections

--- include/boost/geometry/algorithms/detail/is_valid/has_valid_self_turns.hpp
+++ include/boost/geometry/algorithms/detail/is_valid/has_valid_self_turns.hpp
@@ -86,11 +86,13 @@ public:
                 IsAcceptableTurn
             > interrupt_policy;
 
+        // Calculate self-turns, skipping adjacent segments
         detail::self_get_turn_points::self_turns<false, turn_policy>(geometry,
                                           strategy,
                                           robust_policy,
                                           turns,
-                                          interrupt_policy);
+                                          interrupt_policy,
+                                          0, true);
 
         if (interrupt_policy.has_intersections)
         {
