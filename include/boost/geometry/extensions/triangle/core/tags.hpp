81e340a3ca11a127a54b594dd7b2412187d01f23
Tinko Bartels <mail@tinkobartels.de>
2019-05-28 15:01:26 +0200

    Initial triangle definitions.

---
+++ include/boost/geometry/extensions/triangle/core/tags.hpp
@@ -0,0 +1,9 @@
+#ifndef BOOST_GEOMETRY_EXTENSIONS_TRIANGLE_CORE_TAGS_HPP
+#define BOOST_GEOMETRY_EXTENSIONS_TRIANGLE_CORE_TAGS_HPP
+
+namespace boost { namespace geometry
+{
+    struct triangle_tag : ring_tag {};
+}} // namespace boost::geometry
+
+#endif // BOOST_GEOMETRY_EXTENSIONS_TRIANGLE_CORE_TAGS_HPP
